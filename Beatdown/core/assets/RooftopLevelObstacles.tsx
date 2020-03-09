9ea306b125dd00722abea372fc55110cc4469a7e
MattKennedy22 <61391961+MattKennedy22@users.noreply.github.com>
2020-03-09 02:11:24 -0230

    Add files via upload

---
+++ Beatdown/core/assets/RooftopLevelObstacles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="RooftopLevelObstacles" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../../../Users/matth/Documents/Schoolwork/Term 5/Software Design/RooftopLevelObstacles.png" width="96" height="96"/>
+</tileset>
