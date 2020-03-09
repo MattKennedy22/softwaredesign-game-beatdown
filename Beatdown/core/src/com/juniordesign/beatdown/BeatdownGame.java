617f671f17f3aa765605c348d92cbc2dac9bc1ee
AkselGabe <agsorensen@mun.ca>
2020-03-09 20:53:33 -0230

    Menu State Updated Gabe

--- Beatdown/core/src/com/juniordesign/beatdown/BeatdownGame.java
+++ Beatdown/core/src/com/juniordesign/beatdown/BeatdownGame.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
@@ -15,8 +16,8 @@ import com.juniordesign.beatdown.managers.GameStateManager;
 public class BeatdownGame extends ApplicationAdapter {
 
 	public static final String title = "Beatdown";
-	public static final int v_width = 256;
-	public static final int v_height = 144;
+	public static final int v_width = 256; //256
+	public static final int v_height = 144;//144
 	public static final int scale = 4;
 
 	private SpriteBatch batch;
@@ -41,6 +42,7 @@ public class BeatdownGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		//background = new Texture("NesoFhH.png");
 
 		camera = new OrthographicCamera(v_width, v_height);
 		camera.setToOrtho(false, v_width, v_height);
@@ -48,9 +50,8 @@ public class BeatdownGame extends ApplicationAdapter {
 
 		gsm = new GameStateManager(this);
 
-		tiledMap = new TmxMapLoader().load("test1.tmx");
-		tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-
+		//tiledMap = new TmxMapLoader().load("test1.tmx"); //GABE CHANGED THIS FROM test1.tmx
+		//tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 		//player = new Dewey();
 		//player.setPosition(64, 32);
 	}
@@ -60,12 +61,15 @@ public class BeatdownGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		tiledMapRenderer.setView(camera);
-		tiledMapRenderer.render();
+		//batch.begin();
+		//batch.draw(background,0,0);
+		//batch.end();
+		//tiledMapRenderer.setView(camera);
+		//tiledMapRenderer.render();
 		// Updates the game while taking into account when the last time render was called
+
 		gsm.update(Gdx.graphics.getDeltaTime());
 		gsm.draw();
-
 		//batch.setProjectionMatrix(camera.combined);
 		//batch.begin();
 		//player.draw(batch);
@@ -76,8 +80,9 @@ public class BeatdownGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		gsm.dispose();
-		tiledMap.dispose();
+		//tiledMap.dispose();
 		//player.dispose();
+		//background.dispose();
 	}
 
 }
