db8de202b4e0fefbbac057f885da8b30c3ffca3a
MattKennedy22 <61391961+MattKennedy22@users.noreply.github.com>
2020-04-02 23:53:56 -0230

    aaa

--- Beatdown/core/src/com/juniordesign/beatdown/entities/Dewey.java
+++ Beatdown/core/src/com/juniordesign/beatdown/entities/Dewey.java
@@ -36,7 +36,7 @@ public abstract class Dewey {
 
         currentState = State.IDLE;
         runSpeed = 1f; //pixels per second
-        health = 100;
+        health = 10;
 
         animationTimeEnd = 64f / runSpeed;
         animationTime = 0;
